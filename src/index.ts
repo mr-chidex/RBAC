adb942127fde24210a4860bb724b9f5b4c150f85
mr-chidex <mbahchiboy2@gmail.com>
2024-01-05 12:14:58 +0100

    db connected and logger added for error login 🚀

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
+import mongoose from 'mongoose';
+
 import app from './app';
 import config from './config';
+import { logger } from './handlers';
 
 const PORT = config.PORT || 5000;
 
-app.listen(PORT, () => console.log(`server running on PORT:: 🚀💥>>> ${PORT}`));
+// connect database and start server
+mongoose.set('strictQuery', true);
+mongoose
+  .connect(config.DATABASE_URL)
+  .then(() => {
+    console.log('db successfully connected');
+
+    // start server
+    app.listen(PORT, () => console.log(`server running on PORT:: 🚀💥>>> ${PORT}`));
+  })
+  .catch((error) => {
+    logger.log('error', `error connecting to database - ${error}`);
+  });
